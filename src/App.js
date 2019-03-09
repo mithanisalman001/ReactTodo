f358e67c58cd39ccde4d5e8144df0d7c9aedb10d
salman mithani <salmanmithani@salmans-MacBook-Pro.local>
2019-03-09 21:41:10 +0500

    Basic Funtioning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoItems from './TodoItems';
+import './TodoList.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      items : []
+    };
+
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  addItem(e){
+    if (this._inputElement.value !== ""){
+      var newItem = {
+        text : this._inputElement.value,
+        key :  Date.now()
+      };
+
+      this.setState((prevState) => {
+        return{
+          items : prevState.items.concat(newItem)
+        };
+      });
+    }
+      this._inputElement.value = "";
+
+      console.log(this.state.items);
+
+      e.preventDefault();
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}> 
+            <input  ref = {(a) => this._inputElement = a}
+                    placeholder="Enter Task">
+            </input>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        <TodoItems entries={this.state.items} />
       </div>
     );
   }
